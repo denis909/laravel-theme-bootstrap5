66247ab157556e6cae08322db1d81774f0a763f7
denis909 <denis909@mail.ru>
2026-04-15 14:15:27 +0300

    Update checkbox.blade.php

--- components/checkbox.blade.php
+++ components/checkbox.blade.php
@@ -1,10 +1,14 @@
 @props([
     'label' => null,
     'value' => 1,
-    'id' => $id ?? ($name . '-checkbox')
+    'id' => $id ?? ($name . '-checkbox'),
+    'uncheckValue' => null
 ])
 @aware(['error' => null])
 <div class="form-check">
+    @if($uncheckValue !== null)
+        <input type="hidden" name="{{ $attributes->name }}" value="{{ $uncheckValue }}" />
+    @endif
     <input {{ $attributes->merge([
         'class' => $error ? 'is-invalid form-check-input' : 'form-check-input',
         'type' => 'checkbox',

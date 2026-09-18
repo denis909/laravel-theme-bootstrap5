80b7af2099f83584b680a182ffd3eea0abc92aa9
denis909 <denis909@mail.ru>
2026-09-18 18:32:16 +0300

    n/a

--- components/messages.blade.php
+++ components/messages.blade.php
@@ -1,17 +1,34 @@
 @if ($messages = session('status'))
+    {{ session()->forget('status') }}
     @foreach((array) $messages as $message)
         <x-admin::alert type="info" message="{{ $message }}" />
     @endforeach
 @endif
 
 @if ($messages = Session::get('success'))
+    {{ session()->forget('success') }}
     @foreach((array) $messages as $message)
         <x-admin::alert type="success" message="{{ $message }}" />
     @endforeach
 @endif
 
 @if ($messages = Session::get('error'))
+    {{ session()->forget('error') }}
     @foreach((array) $messages as $message)
         <x-admin::alert type="danger" message="{{ $message }}" />
+    @endforeach
+@endif
+
+@if ($messages = Session::get('warning'))
+    {{ session()->forget('warning') }}
+    @foreach((array) $messages as $message)
+        <x-admin::alert type="warning" message="{{ $message }}" />
+    @endforeach
+@endif
+
+@if ($messages = Session::get('info'))
+    {{ session()->forget('info') }}
+    @foreach((array) $messages as $message)
+        <x-admin::alert type="info" message="{{ $message }}" />
     @endforeach
 @endif
